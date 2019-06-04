5c42c4040543ceaba0763f25e354e293f6a5e152
Giuseppe <gcrosti@gmail.com>
2019-06-04 14:58:37 +0000

    passing all tests

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,13 @@
 import requests
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    resp = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    if resp.status_code != 200:
+        raise requests.HTTPError()
+    dict_resp = resp.json()
+    c = 1
+    for gist in dict_resp:
+        db.execute('insert into gists values (?,?,?,?,?,?,?,?,?,?,?,?)',(c,gist['id'],gist['html_url'],gist['git_pull_url'],gist['git_push_url'],gist['commits_url'],gist['forks_url'],gist['public'],gist['created_at'],gist['updated_at'],gist['comments'],gist['comments_url']))
+        c += 1
+    if commit:
+        db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,17 @@
 from .models import Gist
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    l_gists = [Gist(g) for g in db_connection.execute('SELECT * FROM gists')]
+    results = []
+    if not kwargs:
+        return l_gists
+    for gist in l_gists:
+        for key, value in kwargs.items():
+            if type(value) == datetime:
+                cleandatetime = getattr(gist,key).replace('T',' ').replace('Z','')
+                setattr(gist,key,cleandatetime)
+            if getattr(gist,key) == str(value):
+                results.append(gist)
+    return results
+    
